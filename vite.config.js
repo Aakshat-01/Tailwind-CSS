0d8196fc6a39ce566dc3818d491d8bf5371d6af9
Aakshat-01 <aakshatgupta40@gmail.com>
2026-04-08 01:48:03 +0530

    Fixed deployment issue

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        fb: './fb.html',
+        hotel: './hotel.html',
+        landing: './landing.html'
+      }
+    }
+  }
+}
